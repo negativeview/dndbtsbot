9c8244b8ba62a27a5c30ac62f698660fcbdc7c83
Daniel Grace <negativeview@gmail.com>
2015-12-03 04:50:55 +0000

    Add name of macro to PM when you save a macro. Remove all instance of a macro with that name when you delete a macro. Remove a macro before saving the same macro.

--- macro-handler.js
+++ macro-handler.js
@@ -30,35 +30,55 @@ ret.set = function(pieces, message, rawEvent, bot, channelID, globalHandler) {
 		macroName = '!' + macroName;
 	}
 
-	var macroBody = '';
-	for (var i = 2; i < pieces.length; i++) {
-		macroBody += pieces[i] + ' ';
-	}
-	if (macroBody[0] != '!') {
-		macroBody = '!' + macroBody;
-	}
-
-	var newMacro = new ret.macroModel(
-		{
-			name: macroName,
-			macro: macroBody,
-			user: username
-		}
-	);
-	newMacro.save(function(err) {
+	ret.macroModel.find({
+		user: username,
+		name: macroName
+	}).exec(function(err, res) {
 		if (err) {
 			bot.sendMessage({
 				to: username,
-				message: 'Error saving macro: ' + err
+				message: err
 			});
-			return;
-		} else {
-			bot.sendMessage({
-				to: username,
-				message: 'Saved macro.'
-			});
-			return;				
+			return false;	
 		}
+
+		if (res.length) {
+			for (var i = 0; i < res.length; i++) {
+				var result = res[i];
+				result.remove();
+			}
+		}
+
+		var macroBody = '';
+		for (var i = 2; i < pieces.length; i++) {
+			macroBody += pieces[i] + ' ';
+		}
+		if (macroBody[0] != '!') {
+			macroBody = '!' + macroBody;
+		}
+
+		var newMacro = new ret.macroModel(
+			{
+				name: macroName,
+				macro: macroBody,
+				user: username
+			}
+		);
+		newMacro.save(function(err) {
+			if (err) {
+				bot.sendMessage({
+					to: username,
+					message: 'Error saving macro: ' + err
+				});
+				return;
+			} else {
+				bot.sendMessage({
+					to: username,
+					message: 'Saved macro `' + macroName + '`'
+				});
+				return;				
+			}
+		});
 	});
 };
 
@@ -116,8 +136,10 @@ ret.remove = function(pieces, message, rawEvent, bot, channelID, globalHandler) 
 		}
 
 		if (res.length) {
-			var result = res[0];
-			result.remove();
+			for (var i = 0; i < res.length; i++) {
+				var result = res[i];
+				result.remove();
+			}
 		} else {
 			bot.sendMessage({
 				to: username,
